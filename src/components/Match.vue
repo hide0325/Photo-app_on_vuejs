e33636b14b7eb72013230ce8cdb8f27708ab5e76
hide0325 <tmnftc@yahoo.co.jp>
2019-06-02 23:12:33 +0900

    コンポーネント上げるの忘れてた...

---
+++ src/components/Match.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="match">
+    <input v-model="$store.state.matchImg" placeholder="タグで絞り込む">
+  </div>  
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Match',
+  data() {
+    return {
+      matchImg: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.match
+  display flex
+  justify-content space-between
+  width 50%
+  margin auto
+</style>
+
---
+++ src/components/Result.vue
@@ -0,0 +1,52 @@
+<template>
+  <transition>
+    <div class="result" :key="filteredImgs">
+      <div v-for="img in filteredImgs" :key="img.id">
+        <p><i class="fas fa-heart fa-pink"></i>{{ img.likes }}</p>
+        <p><i class="fas fa-tag fa-blue "></i>{{ img.tags }}</p>
+        <a :href="img.pageURL" target="_blank"><img :src="img.previewURL"></a>
+      </div>
+    </div>
+  </transition>  
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Result',
+  computed: {
+    ...mapGetters(['filteredImgs'])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.result
+  display flex
+  flex-wrap wrap
+  width 50%
+  margin auto
+.result div
+  flex-basis 50%
+  text-align center
+.result div ahover
+  opacity .7
+.result div a img
+  width 100%
+  padding 5px
+  box-sizing border-box
+
+.fas
+   margin-right 3px
+.fa-pink
+  color #ff7a8e
+.fa-blue
+  color #be70e6
+
+.v-enter-active
+  transition opacity .8s
+.v-enter
+  opacity 0
+</style>
+
