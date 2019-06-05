470921925c9f63f6adbd7ded61bb8a6531ea1c84
hide0325 <tmnftc@yahoo.co.jp>
2019-06-05 15:30:46 +0900

    ビルド後に何も表示されない問題を解決

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 // import router from './router'
 import store from './store'
 import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
-// import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
-// import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
 import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  publicPath: './',
   chainWebpack: config => {
-      config.module.rules.delete('eslint');
+    config.module.rules.delete('eslint');
   }
 }
