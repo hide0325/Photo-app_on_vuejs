b0a86888c1f802ee76b0483359a08a91741fb51c
hide0325 <tmnftc@yahoo.co.jp>
2019-06-05 14:29:04 +0900

    fontawesome追加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 // import router from './router'
 import store from './store'
+import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+// import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+// import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
 Vue.config.productionTip = false
 
